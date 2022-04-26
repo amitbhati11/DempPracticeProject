2a2ad1ae28d9549bfd2a96623c29fa8f2abcd30f
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-04-26 09:26:01 +0530

    #RestAssured#get#post#put#patch#delete

---
+++ DemoPracticeProject/src/test/java/Restassured/Getnpost.java
@@ -0,0 +1,59 @@
+package Restassured;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import org.json.simple.JSONObject;
+
+public class Getnpost {
+	
+	@DataProvider(name="testpostdata")
+	public Object [][] datapost() {
+		
+		return new  Object   [][] {
+			
+			{"Ankit","Arct"},{"Sourav","Tech"},{"Raquesh","Test13"}
+			
+		};
+		
+		
+	}
+	
+	//@Test(dataProvider="testpostdata")
+	
+	public void postmethod(String name,String job) {
+		baseURI="https://reqres.in";
+		JSONObject req=new JSONObject();
+		req.put("name", name);
+		req.put("job", job);
+		
+		given().contentType(ContentType.JSON).accept(ContentType.JSON)
+		.body(req.toJSONString()).when()
+		.post("/api/users").then()
+		.statusCode(201).log().all();
+		
+	}
+	
+	@Test
+	
+	public void getmethod() {
+		
+		baseURI="https://reqres.in";
+		
+		given().
+		when()
+		.get("/api/users?page=2").
+		then().
+		statusCode(200)
+		.log()
+		.all();
+		
+		
+		
+		
+	}
+
+}
---
+++ DemoPracticeProject/src/test/java/Restassured/putpatchndelete.java
@@ -0,0 +1,77 @@
+package Restassured;
+import static io.restassured.RestAssured.*;
+import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class putpatchndelete {
+	
+	@DataProvider(name="putdata")
+	
+	public Object [][] dataforput(){
+		
+		return new Object [][] {
+			{"Rajvir","test1234"},{"singleton","test5678"},{"lovish","test2390"}
+			
+		};
+	}
+	
+	//@Test(dataProvider="putdata")
+	
+	public void putmethod(String name,String job) {
+		
+		baseURI="https://reqres.in";
+		JSONObject rtp=new JSONObject ();
+		rtp.put("name", name);
+		rtp.put("job", job);
+		
+		given().contentType(ContentType.JSON).accept(ContentType.JSON).body(rtp.toJSONString()).
+		when().put("/api/users/2")
+		.then().statusCode(200).log().all();
+		
+		
+	}
+	
+@DataProvider(name="patchdata")
+	
+	public Object [][] dataforpatch(){
+		
+		return new Object [][] {
+			{"Rajvir","1234"},{"singh","test5678"},{"lov","test2390"}
+			
+		};
+	}
+	
+	//@Test(dataProvider="patchdata")
+	
+	public void patchmethod(String name,String job) {
+		
+		baseURI="https://reqres.in";
+		JSONObject rtp=new JSONObject ();
+		rtp.put("name", name);
+		rtp.put("job", job);
+		
+		given().contentType(ContentType.JSON).accept(ContentType.JSON).body(rtp.toJSONString()).
+		when().patch("/api/users/2")
+		.then().statusCode(200).log().all();
+		
+		
+	}
+	
+	@Test
+	
+	public void deletemethod() {
+		
+		baseURI="https://reqres.in";
+		
+		given().when()
+		.delete("/api/users/2")
+		.then()
+		.statusCode(204).log().all();
+	}
+	
+	
+
+}
